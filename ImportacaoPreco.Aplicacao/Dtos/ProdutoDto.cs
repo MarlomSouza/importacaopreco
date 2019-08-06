1cc72a1b181b78433f5232c670c97173b5f51b04
222406 <marlom1012@gmail.com>
2019-08-06 08:23:28 -0300

    produto service

--- ImportacaoPreco.Aplicacao/Dtos/ProdutoDto.cs
+++ ImportacaoPreco.Aplicacao/Dtos/ProdutoDto.cs
@@ -4,10 +4,10 @@ namespace ImportacaoPreco.Aplicacao.Dtos
 {
     public class ProdutoDto : Dto
     {
-        public int SubgrupoId { get; set; }
+        public string subgrupo { get; set; }
         public IList<string> Tamanhos { get; set; }
 
-        public IList<int> CorId { get; set; }
+        public IList<string> Cores { get; set; }
 
         public decimal Valor_Base { get; set; }
 
--- ImportacaoPreco.Aplicacao/Services/ProdutoService.cs
+++ ImportacaoPreco.Aplicacao/Services/ProdutoService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using ImportacaoPreco.Aplicacao.Dtos;
 using ImportacaoPreco.Dominio.Base;
 using ImportacaoPreco.Dominio.Entities;
@@ -14,14 +16,18 @@ namespace ImportacaoPreco.Aplicacao.Services
 
         public override void Criar(ProdutoDto entityDto)
         {
-            var tamanhos = new[] { new Tamanho("Grande") };
-            var cores = new[] { new Cor("Verde") };
+            var random = new Random();
+            var tamanhos = MapeadorDeTamanho(entityDto.Tamanhos);
+            var cores = MapeadorDeCor(entityDto.Cores);
             var subGrupo = new Subgrupo("Tenis", new Grupo("Calcaçado"));
             var preco = entityDto.Valor_Base;
-            var precoPromocinal = new PrecoPromocional(10, DateTime.Now, DateTime.Now.AddDays(1));
+            var precoPromocinal = new PrecoPromocional(preco * random.Next(), DateTime.Now, DateTime.Now.AddDays(1));
             var precosPromocionais = new[] { precoPromocinal };
             var produto = new Produto(entityDto.Nome, tamanhos, cores, subGrupo, preco, precosPromocionais);
             Criar(produto);
         }
+
+        public static IList<Tamanho> MapeadorDeTamanho(IList<string> tamanhos) => tamanhos.Select(t => new Tamanho(t)).ToList();
+        public static IList<Cor> MapeadorDeCor(IList<string> cores) => cores.Select(c => new Cor(c)).ToList();
     }
 }
